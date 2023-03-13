9de69ee0fe54f674d31e8c970812c70e96c5e21d
Valery <vmarchos19@gmail.com>
2023-03-13 16:28:06 +0300

    fb

--- game.js
+++ game.js
@@ -1,48 +1,30 @@
-var cvs = document.getElementById('canvas');
-var ctx = cvs.getContext('2d');
-
-var bird = new Image();
-var bg = new Image();
-var fg = new Image();
-var pipeUp = new Image();
-var pipeBottom = new Image();
-
-bird.src = 'img/bird-2.png';
-bg.src = 'img/bg.png';
-fg.src = 'img/fg.png';
-pipeUp.src = 'img/pipeUp.png';
-pipeBottom.src = 'img/pipeBottom.png';
-
-var gap = 90;
-document.addEventListener("click", moveUp);
-
-function moveUp() {
-    yPos -= 20;
-}
+const cvs = document.getElementById('canvas');
+const ctx = cvs.getContext('2d');
+//переменные и константы игры
+let frames = 0;
+const sprite = new Image();
+sprite.src = "img/sprite.png"
 
 
-
-
-//позиция птицы
-var xPos = 10;
-var yPos = 150;
-var grav = 1;
-
-
-
+//рисовка
 function draw() {
-    ctx.drawImage(bg, 0, 0);
-    ctx.drawImage(pipeUp, 100, 0)
-    ctx.drawImage(pipeBottom, 100, 0 + pipeUp.height + gap);
-    ctx.drawImage(fg, 0, cvs.height - fg.height)
-    ctx.drawImage(bird, xPos, yPos, 50, 50)
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, cvs.width, cvs.heigth);
 
-    yPos += grav
-    requestAnimationFrame(draw);
 }
 
+//обновление
+function update() {
 
+}
 
+//цикл
+function loop() {
+    update();
+    draw();
+    frames++;
+    requestAnimationFrame(loop);
 
+}
+loop();
 
-pipeBottom.onload = draw;
