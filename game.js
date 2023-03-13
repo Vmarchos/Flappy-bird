7896d70f99fbf43bacc7b326f547838971f78fb0
Valery <vmarchos19@gmail.com>
2023-03-13 16:50:57 +0300

    fb

--- game.js
+++ game.js
@@ -1,15 +1,20 @@
-const cvs = document.getElementById('canvas');
-const ctx = cvs.getContext('2d');
-//переменные и константы игры
-let frames = 0;
+const cvs = document.getElementById("canvas");
+const ctx = cvs.getContext("2d");
+
+
+//переменные и константы
+frames = 0;
+
+//загрузка спрайта
 const sprite = new Image();
 sprite.src = "img/sprite.png"
 
 
 //рисовка
 function draw() {
-    ctx.fillStyle = 'black';
-    ctx.fillRect(0, 0, cvs.width, cvs.heigth);
+    ctx.fillStyle = "#70c5ce";
+    ctx.fillRect(0, 0, cvs.width, cvs.height);
+
 
 }
 
@@ -17,14 +22,12 @@ function draw() {
 function update() {
 
 }
-
 //цикл
 function loop() {
     update();
     draw();
     frames++;
-    requestAnimationFrame(loop);
 
+    requestAnimationFrame(loop);
 }
 loop();
-
