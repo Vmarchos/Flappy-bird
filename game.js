a369756ab25340b1efd1ead2e229ae0e610a96c7
Valery <vmarchos19@gmail.com>
2023-03-14 16:16:36 +0300

    bg

--- game.js
+++ game.js
@@ -10,6 +10,20 @@ const sprite = new Image();
 sprite.src = "img/sprite.png"
 
 
+//бэкграунд
+const bg = {
+    sX: 0,
+    sY: 0,
+    w: 275,
+    h: 225,
+    x: 0,
+    y: cvs.height - 226,
+    draw: function () {
+        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h)
+    }
+}
+
+
 //рисовка
 function draw() {
     ctx.fillStyle = "#70c5ce";
